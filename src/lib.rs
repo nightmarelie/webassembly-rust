909058871931a80b4ff609c63971e99d43154e0d
Oleksii Kryvtsov <nightmarelie@gmail.com>
2024-04-09 22:23:00 -0600

    feat: pass a js code to WebAssembly and invoke it

--- src/lib.rs
+++ src/lib.rs
@@ -3,6 +3,17 @@ pub fn add(left: u32, right: u32) -> u32 {
     left + right
 }
 
+extern {
+    fn appendNumberToBody(number: u32);
+}
+
+#[no_mangle]
+pub fn run() {
+    unsafe {
+        appendNumberToBody(42);
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
